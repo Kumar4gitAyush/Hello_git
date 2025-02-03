14aa0b6b42eaaac4706fa7b981fbdcfdece6b120
Kumar Ayush <kumarayushx@gmail.com>
2025-02-04 01:19:00 +0530

    Added EnumExamples.java

--- index.java
+++ index.java
@@ -1,6 +1,17 @@
-public class index
+
+class Fibo
 {
-    public static void main(String[] args) {
-        System.out.println("Hello Ayush ji");
+    static int fibbo(int num)
+    {
+      if(num==1||num==0)
+      return 0;
+      else
+      return (num-1)+(num-2);
     }
+    
+public static void main(String[] args) {
+        
+    System.out.println("This program prints the fibonacci series for up to five terms");
+    fibbo(5);
+}
 }
